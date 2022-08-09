8a92b9eabce7a237253c5c7d2cdf09e1ccfbccf6
Lee Shinhee <shlee2227@gmail.com>
2022-08-09 23:15:51 +0900

    [ADD]유저 회원가입 및 포스트 작성 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const { sendProducts } = require("./sendProducts");
+const { createUser, createPost } = require("./user");
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "/pong" });
+});
+
+app.post("/signup", createUser);
+app.post("/login", (req, res) => {
+  res.json("login success");
+});
+app.get("/products", sendProducts);
+app.post("/posts", createPost);
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("server is listening on PORT 8000");
+});
